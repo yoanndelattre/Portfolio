d7f0948063470276a588da371ca6ec2a4cb3f0b0
yoanndelattre <yoanndelattre21@gmail.com>
2019-02-17 22:47:14 +0100

    translate formlaire in French

--- src/Pages/Contact/Components/Formulaire.js
+++ src/Pages/Contact/Components/Formulaire.js
@@ -4,13 +4,11 @@ import React from 'react'
 import './Formulaire.css'
 
 export const Formulaire = (  
-    <div className="contact-container">
-        <form className="form">
-            <h2>CONTACT US</h2>
-            <p type="Name:"><input required placeholder="Write your name here.."></input></p>
-            <p type="Email:"><input required placeholder="Let us know how to contact you back.."></input></p>
-            <p type="Message:"><input required placeholder="What would you like to tell us.."></input></p>
-            <button>Send Message</button>
-        </form>
-    </div>
+    <form className="form">
+        <h2>CONTACTEZ-MOI</h2>
+        <p type="Nom:"><input required></input></p>
+        <p type="Email:"><input required></input></p>
+        <p type="Message:"><input required></input></p>
+        <button>Envoyer</button>
+    </form>
 )
