04429a14a215a929b530661581f11a00afd0df74
yoanndelattre <yoanndelattre21@gmail.com>
2019-02-17 19:38:44 +0100

    add specific color url

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -4,20 +4,60 @@ import React, { Component } from 'react'
 import './Navbar.css'
 
 class Navbar extends Component {
+
     render () {
-        return(
-            <header>
-                <div className="link">
-                    <h1>Portfolio</h1>
-                    <a href="Contact" >Contact</a>
-                    <a href="/Projets" >Projets</a>
-                    <a href="/" >Home</a>
-                    <a className='smart-screen' href="/" >Home</a>
-                    <a className='smart-screen' href="/Projets" >Projets</a>
-                    <a className='smart-screen' href="Contact" >Contact</a>
-                </div>
-            </header>
-        )
+
+        // Home
+        if(window.location.pathname === "/") {
+            return (
+                <header>
+                    <div className="link">
+                        <h1>Portfolio</h1>
+                        <a href="/Contact" >Contact</a>
+                        <a href="/Projets" >Projets</a>
+                        <a style={{backgroundColor: '#A4A4A4'}} href="/" >Home</a>
+                        <a style={{backgroundColor: '#A4A4A4'}} className='smart-screen' href="/" >Home</a>
+                        <a className='smart-screen' href="/Projets" >Projets</a>
+                        <a className='smart-screen' href="/Contact" >Contact</a>
+                    </div>
+                </header>
+            )
+        }
+
+        // Projects
+        if(window.location.pathname === "/Projets") {
+            return (
+                <header>
+                    <div className="link">
+                        <h1>Portfolio</h1>
+                        <a href="/Contact" >Contact</a>
+                        <a style={{backgroundColor: '#A4A4A4'}} href="/Projets" >Projets</a>
+                        <a href="/" >Home</a>
+                        <a className='smart-screen' href="/" >Home</a>
+                        <a style={{backgroundColor: '#A4A4A4'}} className='smart-screen' href="/Projets" >Projets</a>
+                        <a className='smart-screen' href="/Contact" >Contact</a>
+                    </div>
+                </header>
+            )
+        }
+
+        
+        // Contact
+        if(window.location.pathname === "/Contact") {
+            return (
+                <header>
+                    <div className="link">
+                        <h1>Portfolio</h1>
+                        <a style={{backgroundColor: '#A4A4A4'}} href="/Contact" >Contact</a>
+                        <a href="/Projets" >Projets</a>
+                        <a href="/" >Home</a>
+                        <a className='smart-screen' href="/" >Home</a>
+                        <a className='smart-screen' href="/Projets" >Projets</a>
+                        <a style={{backgroundColor: '#A4A4A4'}} className='smart-screen' href="/Contact" >Contact</a>
+                    </div>
+                </header>
+            )
+        }
     }
 }
 
