2cff41ae07ea17fa8a3ec1b8bd909641127b9f8a
yoanndelattre <yoanndelattre21@gmail.com>
2019-02-18 18:41:21 +0100

    add sys send mail

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import Navbar from '../../Navbar/Navbar'
-import { Formulaire } from './Components/Formulaire'
+import Formulaire from './Components/Formulaire'
 
 
 class Contact extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault()
+
+        const { name, email, message } = this.state
+
+        const form = await axios.post('/api/form', {
+            name,
+            email,
+            message
+        })
+    }
+
     render () {
         return (
             <div>
                 <Navbar/>
-                {Formulaire}
+                <Formulaire handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
             </div>
         )
     }
