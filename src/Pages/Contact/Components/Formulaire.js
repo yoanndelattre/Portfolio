1022ac291f69053be04619e918ab6f5d27b75a06
yoanndelattre <yoanndelattre21@gmail.com>
2019-02-18 19:53:02 +0100

    add catpcha in formulaire

--- src/Pages/Contact/Components/Formulaire.js
+++ src/Pages/Contact/Components/Formulaire.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 
+// Captcha
+import ReCAPTCHA from "react-google-recaptcha";
+
 //css
 import './Formulaire.css'
 
 export default class Formulaire extends Component {
+
+    state = {
+        displaySubmit: 'none',
+        displayCapcha: 'inline-block'
+    }
+
+    onChange = () => {
+        setTimeout(() => {
+            this.setState({ displaySubmit: 'block' })
+            this.setState({ displayCapcha: 'none' }) 
+        }, 1000);
+    }
+
     render () {
         return (
             <form onSubmit={this.props.handleSubmit} className="form">
@@ -11,7 +27,8 @@ export default class Formulaire extends Component {
                 <p type="Nom:"><input required name="name" onChange={this.props.handleChange} ></input></p>
                 <p type="Email:"><input required name="email" onChange={this.props.handleChange} ></input></p>
                 <p type="Message:"><input required name="message" onChange={this.props.handleChange} ></input></p>
-                <input type="submit" value="Envoyer" />
+                <input style={{ display: this.state.displaySubmit }} type="submit" value="Envoyer" />
+                <ReCAPTCHA style={{ display: this.state.displayCapcha }} onChange={this.onChange} sitekey="6LcSSJIUAAAAAL5q0Z-IT9INdd5dEjq_XgVGTGgG"/>
             </form>
         )
     }
