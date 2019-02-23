9d3d44eed43fc1c133eb947cf98b58391cf76225
Yoann Delattre <yoanndelattre21@hmail.com>
2019-02-23 14:30:08 +0100

    feat(ProjectPage):add info font card project3

--- src/Pages/Projects/Components/ProjectsCards/ProjectCard3.js
+++ src/Pages/Projects/Components/ProjectsCards/ProjectCard3.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import { FlippingCard, FlippingCardBack, FlippingCardFront, UserCard } from 'react-ui-cards'
+import LinkGithubProject from '../Link-Github-Project'
+import LinkWebsiteProject from '../Link-Website-Project'
+
+// img
+import logo_website from '../logo/logo-website.png'
+import capture_markdown_editor from '../img/capture-markdown-editor.png'
 
 // css 
 import './ProjectsCards.css'
@@ -8,13 +14,17 @@ export const ProjectCard3 = (
     <FlippingCard className="ProjectCard3" >
 			<FlippingCardFront>
 				<UserCard 
-					name="Projet3"  
+					avatar={logo_website} 
+					header={capture_markdown_editor}
+					name="Traducteur Markdown" 
+					positionName={<p className="PositionName">Site web traducteur instantané de markdown.</p>} 
 					cardClass="FrontCard FrontProjectCard3" 
 				/>
   	  </FlippingCardFront>
       <FlippingCardBack>
 				<div className="BackCard FrontCard BackProjectCard3">
-					Back
+					<LinkGithubProject LinkGithub={"https://github.com/yoanndelattre/Markdown-Editor"} />
+					<LinkWebsiteProject LinkWebsite={"https://visualizer-docker.yoanndelattre.com"}/>
 				</div>
   	  </FlippingCardBack>
     </FlippingCard>
