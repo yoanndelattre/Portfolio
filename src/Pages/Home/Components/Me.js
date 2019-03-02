54d4d5e693ef7893b4874d00af03b24c2d12d572
yoanndelattre <yoanndelattre21@gmail.com>
2019-03-02 19:03:36 +0100

    feat(HomePage):add logo app on devtools

--- src/Pages/Home/Components/Me.js
+++ src/Pages/Home/Components/Me.js
@@ -7,7 +7,26 @@ import Photo_Profil from '../img/Photo-Profil.jpg'
 import twitter_btn from './logo/twitter.png'
 import linkedin_btn from './logo/linkedin.png'
 import github_btn from './logo/github.png'
+
 import vscode_logo from './logo/devtools/VSCode.png'
+import photoshop_logo from './logo/devtools/photoshop.png'
+import git_logo from './logo/devtools/git.png'
+import chocolatey_logo from './logo/devtools/chocolatey.png'
+import docker_toolbox_logo from './logo/devtools/docker_toolbox.png'
+import kitematic_logo from './logo/devtools/kitematic.svg'
+import hamachi_logo from './logo/devtools/hamachi.png'
+import nodejs_logo from './logo/devtools/nodejs.png'
+import virtualbox_logo from './logo/devtools/virtualbox.png'
+import vmware_logo from './logo/devtools/vmware.png'
+import ungit_logo from './logo/devtools/ungit.png'
+import react_logo from './logo/devtools/react.png'
+import todoist_logo from './logo/devtools/todoist.png'
+import trello_logo from './logo/devtools/trello.png'
+import zapier_logo from './logo/devtools/zapier.png'
+import heroku_logo from './logo/devtools/heroku.png'
+import netlify_logo from './logo/devtools/netlify.png'
+import codeanywhere_logo from './logo/devtools/codeanywhere.png'
+import google_domains_logo from './logo/devtools/google_domains.png'
 
 // css
 import './Me.css'
@@ -42,26 +61,26 @@ class Me extends Component {
                     <div className="devtools-me_card">
                         <h2>Mes outils de travail</h2>
                         <h3>Logiciels :</h3>
-                        <p><a href="https://code.visualstudio.com" target="blank">Visual Code Studio</a><img src={vscode_logo} alt="vscode logo" /></p>
-                        <p><a href="https://www.adobe.com/fr/products/photoshop.html" target="blank">Photoshop CC 2019</a></p>
-                        <p><a href="https://git-scm.com" target="blank">Git</a></p>
-                        <p><a href="https://chocolatey.org" target="blank">Chocolatey</a></p>
-                        <p><a href="https://github.com/docker/toolbox/releases" target="blank">Docker Toolbox</a></p>
-                        <p><a href="https://kitematic.com" target="blank">Kitematic</a></p>
-                        <p><a href="https://www.vpn.net" target="blank">Logmein Hamachi</a></p>
-                        <p><a href="https://nodejs.org" target="blank">NodeJs</a></p>
-                        <p><a href="https://www.virtualbox.org" target="blank">VirtualBox</a></p>
-                        <p><a href="https://www.vmware.com" target="blank">VmWare</a></p>
-                        <p><a href="https://github.com/FredrikNoren/ungit" target="blank">ungit</a></p>
-                        <p><a href="https://facebook.github.io/create-react-app" target="blank">create-react-app</a></p>
+                        <p><a href="https://code.visualstudio.com" target="blank">Visual Code Studio</a><img className="logo" src={vscode_logo} alt="vscode logo"/></p>
+                        <p><a href="https://www.adobe.com/fr/products/photoshop.html" target="blank">Photoshop CC 2019</a><img className="logo" src={photoshop_logo} alt="photoshop logo"/></p>
+                        <p><a href="https://git-scm.com" target="blank">Git</a><img className="logo" src={git_logo} alt="git logo"/></p>
+                        <p><a href="https://chocolatey.org" target="blank">Chocolatey</a><img className="logo" src={chocolatey_logo} alt="chocolatey logo"/></p>
+                        <p><a href="https://github.com/docker/toolbox/releases" target="blank">Docker Toolbox</a><img className="logo" src={docker_toolbox_logo} alt="docker toolbox logo"/></p>
+                        <p><a href="https://kitematic.com" target="blank">Kitematic</a><img className="logo" src={kitematic_logo} alt="kitematic logo"/></p>
+                        <p><a href="https://www.vpn.net" target="blank">Logmein Hamachi</a><img className="logo" src={hamachi_logo} alt="hamachi logo"/></p>
+                        <p><a href="https://nodejs.org" target="blank">NodeJs</a><img className="logo" src={nodejs_logo} alt="nodejs logo"/></p>
+                        <p><a href="https://www.virtualbox.org" target="blank">VirtualBox</a><img className="logo" src={virtualbox_logo} alt="virtualbox logo"/></p>
+                        <p><a href="https://www.vmware.com" target="blank">VmWare</a><img className="logo" src={vmware_logo} alt="vmware logo"/></p>
+                        <p><a href="https://github.com/FredrikNoren/ungit" target="blank">ungit</a><img className="logo" src={ungit_logo} alt="ungit logo"/></p>
+                        <p><a href="https://facebook.github.io/create-react-app" target="blank">create-react-app</a><img className="logo" src={react_logo} alt="react logo"/></p>
                         <h3>Site Web :</h3>
-                        <p><a href="https://todoist.com" target="blank">Todoist</a></p>
-                        <p><a href="https://trello.com" target="blank">Trello</a></p>
-                        <p><a href="https://zapier.com" target="blank">Zapier</a></p>
-                        <p><a href="https://heroku.com" target="blank">Heroku</a></p>
-                        <p><a href="https://netlify.com" target="blank">Netlify</a></p>
-                        <p><a href="https://codeanywhere.com" target="blank">Codeanywhere</a></p>
-                        <p><a href="https://domains.google.com" target="blank">Google Domains</a></p>
+                        <p><a href="https://todoist.com" target="blank">Todoist</a><img className="logo" src={todoist_logo} alt="todoist logo"/></p>
+                        <p><a href="https://trello.com" target="blank">Trello</a><img className="logo" src={trello_logo} alt="trello logo"/></p>
+                        <p><a href="https://zapier.com" target="blank">Zapier</a><img className="logo" src={zapier_logo} alt="zapier logo"/></p>
+                        <p><a href="https://heroku.com" target="blank">Heroku</a><img className="logo" src={heroku_logo} alt="heroku logo"/></p>
+                        <p><a href="https://netlify.com" target="blank">Netlify</a><img className="logo" src={netlify_logo} alt="netlify logo"/></p>
+                        <p><a href="https://codeanywhere.com" target="blank">Codeanywhere</a><img className="logo" src={codeanywhere_logo} alt="codeanywhere logo"/></p>
+                        <p><a href="https://domains.google.com" target="blank">Google Domains</a><img className="logo" src={google_domains_logo} alt="google domains logo"/></p>
                     </div>
                 </div>
             </Fragment>
