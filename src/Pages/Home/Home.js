9e3cb0926e18864bb89ab28fb18ad05a08f54dc1
yoanndelattre <yoanndelattre21@gmail.com>
2019-03-03 17:07:32 +0100

    feat(HomePage): adapt HomePage interface for mobile

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -11,9 +11,21 @@ import './Home.css'
 import Mainbackground from './img/Mainbackground.jpg'
 
 class Home extends Component {
+
+    state = {
+        backgroundImage: `url(${ Mainbackground })`,
+        backgroundColor: 'none'
+    }
+
+    componentDidMount () {
+        if (window.innerWidth <= 768) {
+            this.setState({ backgroundImage: "none", backgroundColor: 'rgb(107, 107, 107)' })
+        }
+    }
+
     render () {
         return (
-            <div className="mainContainer" style={{ backgroundImage: `url(${ Mainbackground })` }}>
+            <div className="mainContainer" style={{ backgroundImage: this.state.backgroundImage, backgroundColor: this.state.backgroundColor }}>
                 <Navbar/>
                 <span className="version">v1.0</span>
                 <div className="home_card">
