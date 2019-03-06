c6de8ebcfa8ff2dfc11d19cdd30c54d2d325c78c
yoanndelattre <yoanndelattre21@gmail.com>
2019-03-06 21:30:07 +0100

    feat(ColorMode): delete state option

--- src/Navbar/Components/SliderColorMode.js
+++ src/Navbar/Components/SliderColorMode.js
@@ -8,35 +8,21 @@ import moon_ico from './ico/moon.png'
 
 class SliderColorMode extends Component {
 
-    state = {
-        DarkMode: '',
-    }
-
-    componentDidMount () {
-        if (localStorage.getItem('DarkModeStorage')) {
-          this.setState({ DarkMode: localStorage.getItem('DarkModeStorage') })
-        }
-        
-        else  {
-            this.setState ({ DarkMode: false })
-        }
-    }
-
-    componentDidUpdate () {
-        localStorage.setItem('DarkModeStorage', this.state.DarkMode)
-    }
-    
     handleChange = (event) => {
-        this.setState({ DarkMode: event.target.checked })
+        if (event.target.checked === true) {
+            localStorage.setItem('DarkMode', true)
+        }
+        if (event.target.checked === false) {
+            localStorage.removeItem('DarkMode')
+        }
     }
-
 
     render () {
         return (
             <Fragment>
                 <img className="ico-sun" src={sun_ico} alt="sun mode" />
                 <label className="switch">
-                    <input defaultChecked={this.state.DarkMode} onChange={this.handleChange} type="checkbox"/>
+                    <input defaultChecked={localStorage.getItem('DarkMode')} onChange={this.handleChange} type="checkbox"/>
                     <span className="slider"></span>
                 </label>
                 <img className="ico-moon" src={moon_ico} alt="sun mode" />
