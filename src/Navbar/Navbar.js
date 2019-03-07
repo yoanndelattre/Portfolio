d02ae4148ea40275f4299fd0668d464aa44cb8d9
yoanndelattre <yoanndelattre21@gmail.com>
2019-03-07 13:08:38 +0100

    feat(ColorMode): add SliderColorMode in Navbar component

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react'
-import SliderColorMode from './Components/SliderColorMode'
+import React, { Component, Fragment } from 'react'
 
 //css
 import './Navbar.css'
+import './SliderColorMode.css'
+
+// ico
+import sun_ico from './ico/sun.png'
+import moon_ico from './ico/moon.png'
 
 class Navbar extends Component {
 
+    handleChange = (event) => {
+        if (event.target.checked === true) {
+            localStorage.setItem('DarkMode', true)
+        }
+        if (event.target.checked === false) {
+            localStorage.removeItem('DarkMode')
+        }
+    }
+
     render () {
+
+        const SliderColorMode = (
+            <Fragment>
+                <img className="ico-sun" src={sun_ico} alt="sun mode" />
+                <label className="switch">
+                    <input defaultChecked={localStorage.getItem('DarkMode')} onChange={this.handleChange} type="checkbox"/>
+                    <span className="slider"></span>
+                </label>
+                <img className="ico-moon" src={moon_ico} alt="sun mode" />
+            </Fragment>
+        )
 
         // Home
         if(window.location.pathname === "/") {
@@ -15,7 +39,7 @@ class Navbar extends Component {
                     <div className="link">
                         <h1>Portfolio</h1>
                         <div className="switch-color">
-                            <SliderColorMode/>
+                            {SliderColorMode}
                         </div>
                         <a href="/Contact" >Contact</a>
                         <a href="/Projets" >Projets</a>
@@ -24,7 +48,7 @@ class Navbar extends Component {
                         <a className='smart-screen' href="/Projets" >Projets</a>
                         <a className='smart-screen' href="/Contact" >Contact</a>
                         <div className="switch-color-smart">
-                            <SliderColorMode/>
+                        {SliderColorMode}
                         </div>
                     </div>
                 </header>
@@ -38,7 +62,7 @@ class Navbar extends Component {
                     <div className="link">
                         <h1>Portfolio</h1>
                         <div className="switch-color">
-                            <SliderColorMode/>
+                            {SliderColorMode}
                         </div>
                         <a href="/Contact" >Contact</a>
                         <a style={{backgroundColor: '#A4A4A4'}} href="/Projets" >Projets</a>
@@ -47,7 +71,7 @@ class Navbar extends Component {
                         <a style={{backgroundColor: '#A4A4A4'}} className='smart-screen' href="/Projets" >Projets</a>
                         <a className='smart-screen' href="/Contact" >Contact</a>
                         <div className="switch-color-smart">
-                            <SliderColorMode/>
+                            {SliderColorMode}
                         </div>
                     </div>
                 </header>
@@ -62,7 +86,7 @@ class Navbar extends Component {
                     <div className="link">
                         <h1>Portfolio</h1>
                         <div className="switch-color">
-                            <SliderColorMode/>
+                            {SliderColorMode}
                         </div>
                         <a style={{backgroundColor: '#A4A4A4'}} href="/Contact" >Contact</a>
                         <a href="/Projets" >Projets</a>
@@ -71,7 +95,7 @@ class Navbar extends Component {
                         <a className='smart-screen' href="/Projets" >Projets</a>
                         <a style={{backgroundColor: '#A4A4A4'}} className='smart-screen' href="/Contact" >Contact</a>
                         <div className="switch-color-smart">
-                            <SliderColorMode/>
+                            {SliderColorMode}
                         </div>
                     </div>
                 </header>
