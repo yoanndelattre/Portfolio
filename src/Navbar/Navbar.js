d0fa5f91cf0c44871b76bae757169fec53b1875e
yoanndelattre <yoanndelattre21@gmail.com>
2019-03-10 18:50:24 +0100

    feat(ColorMode): add hover effects

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -18,6 +18,9 @@ class Navbar extends Component {
         backgroundTextFocusHome: '',
         backgroundTextFocusProjects: '',
         backgroundTextFocusContact: '',
+        backgroundTextHoverHome: '',
+        backgroundTextHoverProjects: '',
+        backgroundTextHoverContact: '',
 
     }
 
@@ -64,6 +67,36 @@ class Navbar extends Component {
         }
     }
 
+    OnMouseEnterHome = () => {
+        if (localStorage.getItem('DarkMode') === "true") {
+            this.setState ({ backgroundTextHoverHome: "rgb(165, 165, 165)" })
+        }
+
+        if (localStorage.getItem('DarkMode') === null) {
+            this.setState ({ backgroundTextHoverHome: "rgb(107, 107, 107)" })
+        }
+    }
+
+    OnMouseEnterProjects = () => {
+        if (localStorage.getItem('DarkMode') === "true") {
+            this.setState ({ backgroundTextHoverProjects: "rgb(165, 165, 165)" })
+        }
+
+        if (localStorage.getItem('DarkMode') === null) {
+            this.setState ({ backgroundTextHoverProjects: "rgb(107, 107, 107)" })
+        }
+    }
+
+    OnMouseEnterContact = () => {
+        if (localStorage.getItem('DarkMode') === "true") {
+            this.setState ({ backgroundTextHoverContact: "rgb(165, 165, 165)" })
+        }
+
+        if (localStorage.getItem('DarkMode') === null) {
+            this.setState ({ backgroundTextHoverContact: "rgb(107, 107, 107)" })
+        }
+    }
+
     render () {
 
         const SliderColorMode = (
@@ -86,12 +119,12 @@ class Navbar extends Component {
                         <div className="switch-color">
                             {SliderColorMode}
                         </div>
-                        <a style={{ color: this.state.ColorText }} href="/Contact" >Contact</a>
-                        <a style={{ color: this.state.ColorText }} href="/Projets" >Projets</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverContact }} onMouseEnter={this.OnMouseEnterContact} onMouseLeave={() => {this.setState ({ backgroundTextHoverContact: "" })}} href="/Contact" >Contact</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverProjects }} onMouseEnter={this.OnMouseEnterProjects} onMouseLeave={() => {this.setState ({ backgroundTextHoverProjects: "" })}} href="/Projets" >Projets</a>
                         <a style={{ color: this.state.ColorTextFocus, background: this.state.backgroundTextFocusHome}} href="/" >Home</a>
                         <a style={{ color: this.state.ColorTextFocus, background: this.state.backgroundTextFocusHome}} className='smart-screen' href="/" >Home</a>
-                        <a style={{ color: this.state.ColorText }} className='smart-screen' href="/Projets" >Projets</a>
-                        <a style={{ color: this.state.ColorText }} className='smart-screen' href="/Contact" >Contact</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverProjects }} onMouseEnter={this.OnMouseEnterProjects} onMouseLeave={() => {this.setState ({ backgroundTextHoverProjects: "" })}} className='smart-screen' href="/Projets" >Projets</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverContact }} onMouseEnter={this.OnMouseEnterContact} onMouseLeave={() => {this.setState ({ backgroundTextHoverContact: "" })}} className='smart-screen' href="/Contact" >Contact</a>
                         <div style={{ background: this.state.backgroundNavBarSmart }} className="switch-color-smart">
                         {SliderColorMode}
                         </div>
@@ -109,12 +142,12 @@ class Navbar extends Component {
                         <div className="switch-color">
                             {SliderColorMode}
                         </div>
-                        <a style={{ color: this.state.ColorText }} href="/Contact" >Contact</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverContact }} onMouseEnter={this.OnMouseEnterContact} onMouseLeave={() => {this.setState ({ backgroundTextHoverContact: "" })}} href="/Contact" >Contact</a>
                         <a style={{ color: this.state.ColorTextFocus, background: this.state.backgroundTextFocusProjects}} href="/Projets" >Projets</a>
-                        <a style={{ color: this.state.ColorText }} href="/" >Home</a>
-                        <a style={{ color: this.state.ColorText }} className='smart-screen' href="/" >Home</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverHome }} onMouseEnter={this.OnMouseEnterHome} onMouseLeave={() => {this.setState ({ backgroundTextHoverHome: "" })}} href="/" >Home</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverHome }} onMouseEnter={this.OnMouseEnterHome} onMouseLeave={() => {this.setState ({ backgroundTextHoverHome: "" })}} className='smart-screen' href="/" >Home</a>
                         <a style={{ color: this.state.ColorTextFocus, background: this.state.backgroundTextFocusProjects }} className='smart-screen' href="/Projets" >Projets</a>
-                        <a style={{ color: this.state.ColorText }} className='smart-screen' href="/Contact" >Contact</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverContact }} onMouseEnter={this.OnMouseEnterContact} onMouseLeave={() => {this.setState ({ backgroundTextHoverContact: "" })}} className='smart-screen' href="/Contact" >Contact</a>
                         <div style={{ background: this.state.backgroundNavBarSmart }} className="switch-color-smart">
                             {SliderColorMode}
                         </div>
@@ -134,10 +167,10 @@ class Navbar extends Component {
                             {SliderColorMode}
                         </div>
                         <a style={{ color: this.state.ColorTextFocus, background: this.state.backgroundTextFocusContact }} href="/Contact" >Contact</a>
-                        <a style={{ color: this.state.ColorText }} href="/Projets" >Projets</a>
-                        <a style={{ color: this.state.ColorText }} href="/" >Home</a>
-                        <a style={{ color: this.state.ColorText }} className='smart-screen' href="/" >Home</a>
-                        <a style={{ color: this.state.ColorText }} className='smart-screen' href="/Projets" >Projets</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverProjects }} onMouseEnter={this.OnMouseEnterProjects} onMouseLeave={() => {this.setState ({ backgroundTextHoverProjects: "" })}} href="/Projets" >Projets</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverHome }} onMouseEnter={this.OnMouseEnterHome} onMouseLeave={() => {this.setState ({ backgroundTextHoverHome: "" })}} href="/" >Home</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverHome }} onMouseEnter={this.OnMouseEnterHome} onMouseLeave={() => {this.setState ({ backgroundTextHoverHome: "" })}} className='smart-screen' href="/" >Home</a>
+                        <a style={{ color: this.state.ColorText, background: this.state.backgroundTextHoverProjects }} onMouseEnter={this.OnMouseEnterProjects} onMouseLeave={() => {this.setState ({ backgroundTextHoverProjects: "" })}} className='smart-screen' href="/Projets" >Projets</a>
                         <a style={{ color: this.state.ColorTextFocus, background: this.state.backgroundTextFocusContact }} className='smart-screen' href="/Contact" >Contact</a>
                         <div style={{ background: this.state.backgroundNavBarSmart }} className="switch-color-smart">
                             {SliderColorMode}
