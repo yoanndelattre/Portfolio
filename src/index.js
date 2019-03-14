d39758bf599daba37c6da9d8c837f944810670fe
Yoann Delattre <yoanndelattre21@hmail.com>
2019-03-14 18:56:10 +0100

    refactor: delete comment index

--- src/index.js
+++ src/index.js
@@ -8,7 +8,4 @@ import MainRouter from './Routes'
 
 ReactDOM.render(<MainRouter/>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
