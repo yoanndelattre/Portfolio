ea802c7d53741cfd3428b6d7e22d2eff334baaa3
Yoann Delattre <yoanndelattre21@hmail.com>
2019-03-14 20:18:01 +0100

    feat(ProjectPage): replace title repo github link

--- src/Pages/Projects/Components/Link-Github-Project.js
+++ src/Pages/Projects/Components/Link-Github-Project.js
@@ -14,7 +14,7 @@ class LinkGithubProject extends Component {
 					<button type="button" className="repo-github-link-button">
 						<img className="repo-github-link-button_ico" src={github_logo} alt="github logo" />
 						<span className="repo-github-link-button_text">
-							Dépôt GitHub du Projet
+							Code Source
 						</span>
 					</button>
 				</a>
