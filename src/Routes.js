3d33afcc05ea4d4f98f9f06493f34c6629cb1e08
Yoann Delattre <yoanndelattre21@hmail.com>
2019-04-07 10:32:17 +0200

    fix:add console log token

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,7 @@ import NotFound from './Pages/NotFound/NotFound'
 
 export default function MainRouter () {
 
+    console.log(process.env.TOKEN_GOOGLE_ANALYTICS)
     ReactGA.initialize(process.env.TOKEN_GOOGLE_ANALYTICS);
 
     function fireTracking() {
