698464ce2801c3ac4d769a4529615b93812511ec
Yoann Delattre <contact@yoanndelattre.com>
2019-05-18 23:16:23 +0200

    fix(SystemSendMail): change url post

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -88,7 +88,7 @@ class Contact extends Component {
     
             const { name, email, message } = this.state
     
-            const form = await axios.post('https://mail-send.yoanndelattre.com/mail/send', {
+            const form = await axios.post('https://mail-send-jubdpulxea-uc.a.run.app/mail/send', {
                 name,
                 email,
                 message
