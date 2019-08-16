0a522b995b1fdff247e42c5e323eb7a528e3298d
yoanndelattre <yoanndelattre21@gmail.com>
2019-08-16 11:56:21 +0200

    add language US

--- src/Language/location.js
+++ src/Language/location.js
@@ -7,6 +7,9 @@ export function getGeoInfo() {
             if( (response.data.country === 'FR') || (response.data.country === 'BJ') || (response.data.country === 'BF') || (response.data.country === 'CG') || (response.data.country === 'CD') || (response.data.country === 'CI') || (response.data.country === 'GA') || (response.data.country === 'ML') || (response.data.country === 'MC') || (response.data.country === 'NE') || (response.data.country === 'NG') || (response.data.country === 'SN') || (response.data.country === 'TG') ) {
                 localStorage.setItem('language', "FR");
             }
+            else {
+                localStorage.setItem('language', "US");
+            }
         }
     })
     .catch((error) => {
