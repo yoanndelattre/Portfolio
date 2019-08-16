0e183b93be22f9e8d13495f406696c30bd72a5d9
yoanndelattre <yoanndelattre21@gmail.com>
2019-08-16 11:44:52 +0200

    feat: add geolocation function

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {getGeoInfo} from './Language/location'
 
 // Pages
 import Home from './Pages/Home/Home'
@@ -7,15 +8,24 @@ import Projects from './Pages/Projects/Projects'
 import Contact from './Pages/Contact/Contact'
 import NotFound from './Pages/NotFound/NotFound'
 
-export default function MainRouter () {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/Projets" component={Projects}/>
-                <Route exact path="/Contact" component={Contact} />
-                <Route path="*" component={NotFound}/>
-            </Switch>
-        </Router>
-    )
+class MainRouter extends Component {
+
+    componentDidMount() {
+        getGeoInfo()
+    }
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/Projets" component={Projects}/>
+                    <Route exact path="/Contact" component={Contact} />
+                    <Route path="*" component={NotFound}/>
+                </Switch>
+            </Router>
+        )
+    }
 }
+
+export default MainRouter
