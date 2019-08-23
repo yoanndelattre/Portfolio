349d661020d718e83e879bd7f47e4e97f8098cfa
yoanndelattre <yoanndelattre21@gmail.com>
2019-08-23 11:56:47 +0200

    fix(SystemSendMail): remove const form

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -93,13 +93,12 @@ class Contact extends Component {
             const { name, email, message } = this.state
             const languageUser = localStorage.getItem('language')
     
-            const form = await axios.post('https://mail-send-jubdpulxea-uc.a.run.app/mail/send', {
+            await axios.post('https://mail-send-jubdpulxea-uc.a.run.app/mail/send', {
                 name,
                 email,
                 message,
                 languageUser
             })
-            console.log(form)
         }
     }
 
