5bc0d2ab53db2b61958404203bf081684dd4cdd2
yoanndelattre <yoanndelattre21@gmail.com>
2019-09-16 18:40:39 +0200

    fix(Language): remove geolocate function for default french language

--- src/Language/flags-selector.js
+++ src/Language/flags-selector.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import {getGeoInfo} from './location'
 import ReactFlagsSelect from 'react-flags-select'
-import ReactLoading from 'react-loading'
 
 // css
 import 'react-flags-select/css/react-flags-select.css'
@@ -14,19 +12,25 @@ class FlagsSelector extends Component {
         alignOptions: ''
     }
 
-    AutoReloadContactPage () {
-        if(window.location.pathname === "/Contact") {
-            window.location.reload();
-        }
-    }
-
     componentDidMount() {
-        getGeoInfo()
+        this.InitLanguageDefault()
         if (window.innerWidth <= 768) {
             this.setState({
                 selectedSize: '25',
                 alignOptions: 'left'
             })
+        }
+    }
+
+    InitLanguageDefault () {
+        if (localStorage.getItem('language') === null) {
+            localStorage.setItem('language', "FR")
+        }
+    }
+
+    AutoReloadContactPage () {
+        if(window.location.pathname === "/Contact") {
+            window.location.reload();
         }
     }
 
@@ -59,11 +63,7 @@ class FlagsSelector extends Component {
             )
         }
         else {
-            return(
-                <div className='loadingpageflag'>
-                    <ReactLoading type="balls" color={this.colorLoading()} width={60}/>
-                </div>
-            )
+            return(null)
         }
     }
 }
--- src/Pages/Home/Components/Me.js
+++ src/Pages/Home/Components/Me.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 // img
 import Photo_Profil from '../img/Photo-Profil.jpg'
