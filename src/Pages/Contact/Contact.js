bcdf02e512de397e5e187af8a9a158de78116960
yoanndelattre <yoanndelattre21@gmail.com>
2019-10-04 14:37:27 +0200

    fix(SendMail): change post url original

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -93,7 +93,7 @@ class Contact extends Component {
             const { name, email, message } = this.state
             const languageUser = localStorage.getItem('language')
     
-            await axios.post('https://mail-send-beta-jubdpulxea-uc.a.run.app/mail/send', {
+            await axios.post('https://mail-send-jubdpulxea-uc.a.run.app/mail/send', {
                 name,
                 email,
                 message,
