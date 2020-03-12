2ad48430bc94a6bb87d57b0f6d259f34c51a933c
Yoann Delattre <yoanndelattre21@gmail.com>
2020-03-12 12:48:08 +0100

    fix(test): change desciption test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import MainRouter from './Routes'
 
-test('renders learn react link', () => {
+test('renders Home Page', () => {
   const { getByText } = render(<MainRouter/>);
   const linkElement = getByText(/Yoann Delattre | Portfolio/i);
   expect(linkElement).toBeInTheDocument();
