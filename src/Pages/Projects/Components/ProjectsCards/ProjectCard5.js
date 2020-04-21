7d411076346e09df3615c97a15e847f9f0a6b9e8
yoanndelattre <yoanndelattre21@gmail.com>
2020-04-21 10:47:38 +0200

    ProjectsPage: change desciption project 5

--- src/Pages/Projects/Components/ProjectsCards/ProjectCard5.js
+++ src/Pages/Projects/Components/ProjectsCards/ProjectCard5.js
@@ -23,10 +23,10 @@ export default class ProjectCard5 extends Component {
 	
 	SummaryProjectName () {
         if(localStorage.getItem('language') === 'FR') {
-            return('Site web permettant à différents utilisateurs de communiquer sous un pseudo qu’ils auront préalablement indiqué sur la page de connexion.')
+            return('Site web permettant à différents utilisateurs de communiquer sous un pseudo qu’ils auront préalablement indiqué sur la page de connexion. Hébergé par le service Cloud Run sur Google Cloud Platform.')
         }
         else {
-            return('Website allowing different users to communicate under a username they have previously indicated on the login page.')
+            return('Website allowing different users to communicate under a username they have previously indicated on the login page. Hosted by the Cloud Run service on Google Cloud Platform.')
         }
     }
 
