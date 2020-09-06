20d7f794c4c833ced2e61b8a9681d8e9444e442c
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-06 13:54:46 +0200

    ESlint: fix syntax in NotFound.js file

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -8,15 +8,16 @@ const NotFound = () => (
   <div id="notfound">
     <HelmetProvider>
       <Helmet>
-        	<title>404 Error NotFound</title>
-    	</Helmet>
+        <title>404 Error NotFound</title>
+      </Helmet>
     </HelmetProvider>
     <div className="notfound">
       <div className="notfound-404">
         <h1>404</h1>
       </div>
       <h2>Oops! This Page Could Not Be Found</h2>
-      <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+      <p>Sorry but the page you are looking for does not exist.</p>
+      <p>Name changed or is temporarily unavailable</p>
       <a href="/">Go To Homepage</a>
       <a target="blank" href="https://github.com/yoanndelattre/Portfolio/issues">Report a Problem</a>
     </div>
