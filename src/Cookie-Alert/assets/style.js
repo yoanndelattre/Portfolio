9a1636adfa751e454192f79c5121fc499fec031e
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-07 21:23:50 +0200

    CookieBanner: simplify code

---
+++ src/Cookie-Alert/assets/style.js
@@ -0,0 +1,28 @@
+import {darkModeSelecting} from '../../Navbar/Navbar';
+
+export const cookieBannerbackgroundColor = () => {
+  if (darkModeSelecting() === true) {
+    return ('rgb(51, 51, 51)');
+  }
+  if (darkModeSelecting() === false) {
+    return ('rgb(218, 218, 218)');
+  }
+};
+
+export const cookieMessageColor = () => {
+  if (darkModeSelecting() === true) {
+    return ('white');
+  }
+  if (darkModeSelecting() === false) {
+    return ('black');
+  }
+};
+
+export const cookieButtonColor = () => {
+  if (darkModeSelecting() === true) {
+    return ('black');
+  }
+  if (darkModeSelecting() === false) {
+    return ('white');
+  }
+};
