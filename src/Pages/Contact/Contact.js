f5b98beecfa033fb3ac8e0d2dafc89208ca0363f
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-09 11:58:56 +0200

    ESlint: fix syntax indent

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -54,200 +54,200 @@ class Contact extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-    handleChange = (e) => {
-      this.setState({
-        [e.target.name]: e.target.value,
-      });
+  updateColorMode = () => {
+    this.setState({
+      updateColorMode: localStorage.getItem('DarkMode'),
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    if (this.state.verifyCaptcha === 'true') {
+      e.preventDefault(
+          this.setState({
+            loadingSubmit: true,
+          }),
+          this.sendMessage(),
+      );
     }
+  }
 
-    handleSubmit(e) {
-      if (this.state.verifyCaptcha === 'true') {
-        e.preventDefault(
-            this.setState({
-              loadingSubmit: true,
-            }),
-            this.sendMessage(),
-        );
-      }
-    }
+  sendMessage = () => {
+    const {name, email, message} = this.state;
+    const languageUser = flagsSelecting();
 
-    sendMessage = () => {
-      const {name, email, message} = this.state;
-      const languageUser = flagsSelecting();
-
-      axios.post(process.env.REACT_APP_URL_POST, {
-        name,
-        email,
-        message,
-        languageUser,
-      }).then((response) => {
-        const status = response.status;
-        if (status === 200) {
-          this.sendingConfirmationTrue();
-        } else {
-          this.sendingConfirmationFalse();
-        }
-      }).catch( (error) => {
+    axios.post(process.env.REACT_APP_URL_POST, {
+      name,
+      email,
+      message,
+      languageUser,
+    }).then((response) => {
+      const status = response.status;
+      if (status === 200) {
+        this.sendingConfirmationTrue();
+      } else {
         this.sendingConfirmationFalse();
-        console.log(error);
-      });
-    }
+      }
+    }).catch( (error) => {
+      this.sendingConfirmationFalse();
+      console.log(error);
+    });
+  }
 
-    sendingConfirmationTrue = () => {
+  sendingConfirmationTrue = () => {
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+      valueSubmit: '✓',
+      loadingSubmit: false,
+      fontSizeSubmit: '35px',
+      paddingSubmit: '0 6px',
+      borderSubmit: '2px solid green',
+      borderForm: '3px solid green',
+      widthForm: '367px',
+      heightForm: '427px',
+    });
+    this.notificationSpamMailTrue();
+    setTimeout(() => {
+      window.location.reload();
+    }, 15000);
+  }
+
+  sendingConfirmationFalse = () => {
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+      valueSubmit: 'X',
+      loadingSubmit: false,
+      fontSizeSubmit: '35px',
+      paddingSubmit: '0 6px',
+      borderSubmit: '2px solid red',
+      borderForm: '3px solid red',
+      widthForm: '367px',
+      heightForm: '427px',
+    });
+    this.notificationSpamMailFalse();
+    setTimeout(() => {
+      window.location.reload();
+    }, 15000);
+  }
+
+  ThemeCapcha = () => {
+    const TokenKey = '6LcSSJIUAAAAAL5q0Z-IT9INdd5dEjq_XgVGTGgG';
+
+    return (
+      <div style={{display: this.state.displayCapcha}}>
+        <Reaptcha
+          onRender={this.Loading}
+          onVerify={this.onVerify}
+          sitekey={TokenKey}
+        />
+      </div>
+    );
+  }
+
+  onVerify = () => {
+    this.setState({verifyCaptcha: 'true'});
+    setTimeout(() => {
       this.setState({
-        name: '',
-        email: '',
-        message: '',
-        valueSubmit: '✓',
-        loadingSubmit: false,
-        fontSizeSubmit: '35px',
-        paddingSubmit: '0 6px',
-        borderSubmit: '2px solid green',
-        borderForm: '3px solid green',
-        widthForm: '367px',
-        heightForm: '427px',
+        displaySubmit: 'block',
+        displayCapcha: 'none',
       });
-      this.notificationSpamMailTrue();
-      setTimeout(() => {
-        window.location.reload();
-      }, 15000);
+    }, 1000);
+  }
+
+  Loading = () => {
+    this.setState({
+      displayApp: 'block',
+      displayLoading: 'none',
+    });
+  }
+
+  notificationSpamMailTrue = () => {
+    if (flagsSelecting() === 'fr') {
+      new Noty({
+        text: 'Un mail de confirmation vous a été envoyé. Si vous ne le trouvez pas, vérifier dans les courriers indésirables.',
+        theme: 'bootstrap-v4',
+        type: 'success',
+        layout: 'bottomCenter',
+      }).show();
+    } else {
+      new Noty({
+        text: 'A confirmation email has been sent to you. If you can not find it, check in junk mail.',
+        theme: 'bootstrap-v4',
+        type: 'success',
+        layout: 'bottomCenter',
+      }).show();
     }
+  }
 
-    sendingConfirmationFalse = () => {
-      this.setState({
-        name: '',
-        email: '',
-        message: '',
-        valueSubmit: 'X',
-        loadingSubmit: false,
-        fontSizeSubmit: '35px',
-        paddingSubmit: '0 6px',
-        borderSubmit: '2px solid red',
-        borderForm: '3px solid red',
-        widthForm: '367px',
-        heightForm: '427px',
-      });
-      this.notificationSpamMailFalse();
-      setTimeout(() => {
-        window.location.reload();
-      }, 15000);
+  notificationSpamMailFalse = () => {
+    if (flagsSelecting() === 'fr') {
+      new Noty({
+        text: 'Une erreur est survenue. Veuillez réessayer plus tard.',
+        theme: 'bootstrap-v4',
+        type: 'error',
+        layout: 'bottomCenter',
+      }).show();
+    } else {
+      new Noty({
+        text: 'An error has occurred. Please try again later.',
+        theme: 'bootstrap-v4',
+        type: 'error',
+        layout: 'bottomCenter',
+      }).show();
     }
+  }
 
-    ThemeCapcha = () => {
-      const TokenKey = '6LcSSJIUAAAAAL5q0Z-IT9INdd5dEjq_XgVGTGgG';
-
-      return (
-        <div style={{display: this.state.displayCapcha}}>
-          <Reaptcha
-            onRender={this.Loading}
-            onVerify={this.onVerify}
-            sitekey={TokenKey}
+  render() {
+    return (
+      <Fragment>
+        <HelmetProvider>
+          <Helmet>
+            <title>Contact • Yoann Delattre | Portfolio</title>
+          </Helmet>
+          <Helmet htmlAttributes={{lang: languageHtmlTag()}}/>
+        </HelmetProvider>
+        <CookieAlert/>
+        <div className="loadingpage" style={{display: this.state.displayLoading}}>
+          <ReactLoading
+            type="bars"
+            color="#fff"
+            width={'10%'}
           />
         </div>
-      );
-    }
-
-    onVerify = () => {
-      this.setState({verifyCaptcha: 'true'});
-      setTimeout(() => {
-        this.setState({
-          displaySubmit: 'block',
-          displayCapcha: 'none',
-        });
-      }, 1000);
-    }
-
-    Loading = () => {
-      this.setState({
-        displayApp: 'block',
-        displayLoading: 'none',
-      });
-    }
-
-    notificationSpamMailTrue = () => {
-      if (flagsSelecting() === 'fr') {
-        new Noty({
-          text: 'Un mail de confirmation vous a été envoyé. Si vous ne le trouvez pas, vérifier dans les courriers indésirables.',
-          theme: 'bootstrap-v4',
-          type: 'success',
-          layout: 'bottomCenter',
-        }).show();
-      } else {
-        new Noty({
-          text: 'A confirmation email has been sent to you. If you can not find it, check in junk mail.',
-          theme: 'bootstrap-v4',
-          type: 'success',
-          layout: 'bottomCenter',
-        }).show();
-      }
-    }
-
-    notificationSpamMailFalse = () => {
-      if (flagsSelecting() === 'fr') {
-        new Noty({
-          text: 'Une erreur est survenue. Veuillez réessayer plus tard.',
-          theme: 'bootstrap-v4',
-          type: 'error',
-          layout: 'bottomCenter',
-        }).show();
-      } else {
-        new Noty({
-          text: 'An error has occurred. Please try again later.',
-          theme: 'bootstrap-v4',
-          type: 'error',
-          layout: 'bottomCenter',
-        }).show();
-      }
-    }
-
-    updateColorMode = () => {
-      this.setState({
-        updateColorMode: localStorage.getItem('DarkMode'),
-      });
-    }
-
-    render() {
-      return (
-        <Fragment>
-          <HelmetProvider>
-            <Helmet>
-              <title>Contact • Yoann Delattre | Portfolio</title>
-            </Helmet>
-            <Helmet htmlAttributes={{lang: languageHtmlTag()}}/>
-          </HelmetProvider>
-          <CookieAlert/>
-          <div className="loadingpage" style={{display: this.state.displayLoading}}>
-            <ReactLoading
-              type="bars"
-              color="#fff"
-              width={'10%'}
-            />
-          </div>
-          <div className="ContactContainer" style={{display: this.state.displayApp, backgroundColor: backgroundContainer(), height: '100%'}}>
-            <Navbar updateColorMode={this.updateColorMode}/>
-            <Formulaire
-              displaySubmit={this.state.displaySubmit}
-              ThemeCapcha={this.ThemeCapcha}
-              heightForm={this.state.heightForm}
-              widthForm={this.state.widthForm}
-              borderForm={this.state.borderForm}
-              borderSubmit={this.state.borderSubmit}
-              paddingSubmit={this.state.paddingSubmit}
-              fontSizeSubmit={this.state.fontSizeSubmit}
-              valueSubmit={this.state.valueSubmit}
-              loadingSubmit={this.state.loadingSubmit}
-              name={this.state.name}
-              email={this.state.email}
-              message={this.state.message}
-              handleChange={this.handleChange}
-              handleSubmit={this.handleSubmit}
-            />
-            <ShareButton display={displayShareButton()}/>
-            {ReportProblem}
-          </div>
-        </Fragment>
-      );
-    }
+        <div className="ContactContainer" style={{display: this.state.displayApp, backgroundColor: backgroundContainer(), height: '100%'}}>
+          <Navbar updateColorMode={this.updateColorMode}/>
+          <Formulaire
+            displaySubmit={this.state.displaySubmit}
+            ThemeCapcha={this.ThemeCapcha}
+            heightForm={this.state.heightForm}
+            widthForm={this.state.widthForm}
+            borderForm={this.state.borderForm}
+            borderSubmit={this.state.borderSubmit}
+            paddingSubmit={this.state.paddingSubmit}
+            fontSizeSubmit={this.state.fontSizeSubmit}
+            valueSubmit={this.state.valueSubmit}
+            loadingSubmit={this.state.loadingSubmit}
+            name={this.state.name}
+            email={this.state.email}
+            message={this.state.message}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+          />
+          <ShareButton display={displayShareButton()}/>
+          {ReportProblem}
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default Contact;
