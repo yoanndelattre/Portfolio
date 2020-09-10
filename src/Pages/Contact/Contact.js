323987f0393eb8ebf7bd318cb582ccfaac5bc480
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-10 19:10:26 +0200

    MailSend: change axios method

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -96,13 +96,16 @@ class Contact extends Component {
     const {name, email, message} = this.state;
     const languageUser = flagsSelecting();
 
-    axios.post(process.env.REACT_APP_URL_POST,
-        JSON.stringify({
-          name,
-          email,
-          message,
-          languageUser,
-        })).then((response) => {
+    axios({
+      method: 'post',
+      url: process.env.REACT_APP_URL_POST,
+      data: JSON.stringify({
+        name,
+        email,
+        message,
+        languageUser,
+      }),
+    }).then((response) => {
       const status = response.status;
       if (status === 200) {
         this.sendingConfirmationTrue();
