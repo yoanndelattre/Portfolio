163e148e6f68ca92e27b223f6e4062d540670bf6
yoanndelattre <yoanndelattre21@gmail.com>
2021-09-15 11:48:24 +0200

    HomePage: add logos crypto

--- src/Pages/Home/Components/Me.js
+++ src/Pages/Home/Components/Me.js
@@ -36,6 +36,15 @@ import GitlabLogo from './logo/devtools/gitlab.png';
 import HypervLogo from './logo/devtools/hyperv.png';
 import NordvpnLogo from './logo/devtools/nordvpn.png';
 import MediumLogo from './logo/devtools/medium.png';
+import BinanceLogo from './logo/devtools/binance.png';
+import CoinbaseLogo from './logo/devtools/coinbase.png';
+import FTXLogo from './logo/devtools/ftx.png';
+import YieldAppLogo from './logo/devtools/yieldapp.png';
+import MaiarLogo from './logo/devtools/maiar.png';
+import MetamaskLogo from './logo/devtools/metamask.png';
+import PhantomLogo from './logo/devtools/phantom.png';
+import XDEFILogo from './logo/devtools/xdefi.png';
+import CoinMarketCapLogo from './logo/devtools/coinmarketcap.png';
 
 // css
 import './Me.css';
@@ -96,6 +105,17 @@ class Me extends Component {
           <p><a style={{color: colorText()}} href="https://www.docker.com/products/docker-desktop" target="blank">Docker Desktop</a><img className="logo" src={DockerDesktopLogo} alt="docker desktop logo"/></p>
           <p><a style={{color: colorText()}} href="https://docs.microsoft.com/virtualization/hyper-v-on-windows/quick-start/enable-hyper-v" target="blank">Hyper V</a><img className="logo" src={HypervLogo} alt="hyperv logo"/></p>
           <p><a style={{color: colorText()}} href="https://nordvpn.com/features" target="blank">NordVPN</a><img className="logo" src={NordvpnLogo} alt="nordvpn logo"/></p>
+          <hr/>
+          <h3>Crypto :</h3>
+          <p><a style={{color: colorText()}} href="https://code.visualstudio.com" target="blank">Binance</a><img className="logo" src={BinanceLogo} alt="binance logo"/></p>
+          <p><a style={{color: colorText()}} href="https://code.visualstudio.com" target="blank">Coinbase</a><img className="logo" src={CoinbaseLogo} alt="coinbase logo"/></p>
+          <p><a style={{color: colorText()}} href="https://code.visualstudio.com" target="blank">FTX</a><img className="logo" src={FTXLogo} alt="ftx logo"/></p>
+          <p><a style={{color: colorText()}} href="https://code.visualstudio.com" target="blank">Yield App</a><img className="logo" src={YieldAppLogo} alt="yield app logo"/></p>
+          <p><a style={{color: colorText()}} href="https://code.visualstudio.com" target="blank">Maiar</a><img className="logo" src={MaiarLogo} alt="maiar logo"/></p>
+          <p><a style={{color: colorText()}} href="https://code.visualstudio.com" target="blank">Metamask</a><img className="logo" src={MetamaskLogo} alt="metamask logo"/></p>
+          <p><a style={{color: colorText()}} href="https://code.visualstudio.com" target="blank">Phantom</a><img className="logo" src={PhantomLogo} alt="phantom logo"/></p>
+          <p><a style={{color: colorText()}} href="https://code.visualstudio.com" target="blank">XDEFI</a><img className="logo" src={XDEFILogo} alt="xdefi logo"/></p>
+          <p><a style={{color: colorText()}} href="https://code.visualstudio.com" target="blank">CoinMarketCap</a><img className="logo" src={CoinMarketCapLogo} alt="coinmarketcap logo"/></p>
           <hr/>
           <h3>{websiteName()}</h3>
           <p><a style={{color: colorText()}} href="https://todoist.com" target="blank">Todoist</a><img className="logo" src={TodoistLogo} alt="todoist logo"/></p>
