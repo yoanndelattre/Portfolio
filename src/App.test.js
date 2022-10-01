14ce36ff873c5cb7ca7556080f42057eb5296f58
yoanndelattre <yoanndelattre21@gmail.com>
2022-10-01 12:04:10 +0200

    Test: Change return component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {render} from '@testing-library/react';
-import MainRouter from './Routes';
+import Home from './Pages/Home/Home';
 
 test('renders Home Page', () => {
-  const {getByText} = render(<MainRouter/>);
+  const {getByText} = render(<Home/>);
   const linkElement = getByText(/Yoann Delattre | Portfolio/i);
   expect(linkElement).toBeInTheDocument();
 });
